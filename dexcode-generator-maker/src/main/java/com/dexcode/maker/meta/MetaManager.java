a6844fa3525e88d5d38712a4767b6e3e26f36064
Smilexing <2664626327@qq.com>
2024-03-07 20:47:09 +0800

    第五期-2：可扩展性优化

--- dexcode-generator-maker/src/main/java/com/dexcode/maker/meta/MetaManager.java
+++ dexcode-generator-maker/src/main/java/com/dexcode/maker/meta/MetaManager.java
@@ -6,9 +6,9 @@ import cn.hutool.json.JSONUtil;
 public class MetaManager {
 
     // volatile，并发编程中常用的关键字，确保多线程环境下的内存可见性，这样meta一旦被修改，所有内存都能看见
-    private static volatile com.dexcode.maker.meta.Meta meta;
+    private static volatile Meta meta;
 
-    public static com.dexcode.maker.meta.Meta getMetaObject() {
+    public static Meta getMetaObject() {
         if (meta == null) {
             // 加锁
             synchronized (MetaManager.class) {
@@ -20,10 +20,11 @@ public class MetaManager {
         return meta;
     }
 
-    private static com.dexcode.maker.meta.Meta initMeta() {
+    private static Meta initMeta() {
         String metaJson = ResourceUtil.readUtf8Str("meta.json");
-        com.dexcode.maker.meta.Meta newMeta = JSONUtil.toBean(metaJson, com.dexcode.maker.meta.Meta.class);
-        // todo 校验配置文件，处理默认值
+        Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
+        // 校验配置文件，处理默认值
+        MetaValidator.doValidAndFill(newMeta);
         return newMeta;
     }
 }
